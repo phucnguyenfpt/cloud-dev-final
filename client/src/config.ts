d03128ba0a8415aa28f9b9e8bf4aca8326a5b45c
phucnguyenfpt <phucnguyentt32@gmail.com>
2024-06-25 16:11:34 +0700

    final

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '8qfhhekcv2'
+const apiId = '15b1v5rq6l'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-lh5ahho121f1i1r2.us.auth0.com',            // Auth0 domain
+  domain: 'dev-lh5ahho121f1i1r2.us.auth0.com',
   clientId: 'VuZ3iPIQZhkIz1zQVnx92uliL7RTkw7l',
   callbackUrl: 'http://localhost:3000/callback'
 }
